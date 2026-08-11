415709c1efc9f28fde632ee4b7e8b772ae65f62c
mauloajin <mauloa32153@gmail.com>
2026-08-11 17:58:09 +0900

    Add app/sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://teppanyaki-sumiyaki-saito.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1
+    }
+  ];
+}
