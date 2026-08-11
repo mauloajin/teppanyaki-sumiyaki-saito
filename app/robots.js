ffc98bfb93f65155783b614c3ad275b28c80fb0f
mauloajin <mauloa32153@gmail.com>
2026-08-11 17:58:08 +0900

    Add app/robots.js

---
+++ app/robots.js
@@ -0,0 +1,7 @@
+export default function robots() {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://teppanyaki-sumiyaki-saito.vercel.app/sitemap.xml",
+    host: "https://teppanyaki-sumiyaki-saito.vercel.app"
+  };
+}
